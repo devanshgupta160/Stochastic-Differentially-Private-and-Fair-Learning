7854e4446789cfef4c635627077c7f3ccda88def
Devansh Gupta <54774124+devanshgupta160@users.noreply.github.com>
2024-04-16 02:53:36 -0700

    Uploaded the latest code

--- dp_fermi.py
+++ dp_fermi.py
@@ -44,10 +44,10 @@ for args.lr_theta in args.lr_theta_list:
                     torch.manual_seed(model_number)
                     #Setting up new initializations and shuffling
                     if args.tuning:
-                        dataloader_train = Data.DataLoader(dataset_train, batch_size = args.batch_size, shuffle = False, num_workers = 2)
+                        dataloader_train = Data.DataLoader(dataset_train, batch_size = 1, shuffle = False, num_workers = 2)
                         dataloader_test = Data.DataLoader(dataset_test, batch_size = args.batch_size, shuffle = False, num_workers = 2)
                     else:
-                        dataloader_train = Data.DataLoader(dataset_train, batch_size = args.batch_size, shuffle = True, num_workers = 2)
+                        dataloader_train = Data.DataLoader(dataset_train, batch_size = 1, shuffle = True, num_workers = 2)
                         dataloader_test = Data.DataLoader(dataset_test, batch_size = args.batch_size, shuffle = True, num_workers = 2)
 
                     #Model, Loss Critetions, Optimizers, and Parameters
@@ -79,19 +79,21 @@ for args.lr_theta in args.lr_theta_list:
                         P_s_negative_half[0] = P_s_negative_half[0].to(device)
                         P_s_negative_half[1] = P_s_negative_half[1].to(device)
                     
+                    model_grad = {}
+                    for name, param in model.named_parameters():
+                        model_grad[name] = torch.zeros(param.shape).to(device)
+                    
                     for epoch in tqdm(range(args.epochs), f"Lambd: {args.lambd} Model {model_number}"):
                         #Training Loop
+                        counter = 0
                         for batch_no, (non_sensitive, sensitive, label, _) in enumerate(dataloader_train):
+                            ##### Per Sample Gradient Updates ######
                             non_sensitive = non_sensitive.to(device)
                             sensitive = sensitive.to(device)
                             label = label.to(device)
-
+                            
+                            #Resetting the model gradients per sample
                             model.zero_grad()
-                            if W.grad is not None:
-                                W.grad.copy_(torch.zeros(W.shape))
-                            if not args.demographic_parity:
-                                if W_.grad is not None:
-                                    W_.grad.copy_(torch.zeros(W_.shape))
                             
                             y_logit, y_hat = model(non_sensitive.float())
                             
@@ -129,12 +131,12 @@ for args.lr_theta in args.lr_theta_list:
                                 fermi_loss_0 = -1*torch.trace(W_ @ p_hat_yhat_part_0 @ W_.T) + 2*torch.trace(W_ @ p_hat_yhat_s_given_0 @ P_s_negative_half[0]) - 1
                                 fermi_loss = fermi_loss_0 + fermi_loss_1
                             
-                            total_loss = classification_loss + args.lambd * fermi_loss
+                            total_loss = (classification_loss + args.lambd * fermi_loss)/args.batch_size
                             total_loss.backward()
 
                             with torch.no_grad():
                                 grad_norm = 0
-                                for param in model.parameters():
+                                for name, param in model.named_parameters():
                                     grad_norm += torch.norm(param.grad)**2
                                 grad_norm = grad_norm ** 0.5
 
@@ -142,35 +144,57 @@ for args.lr_theta in args.lr_theta_list:
                                 divide_by = grad_norm.item()/args.lipschitz_theta
                                 divide_by = divide_by if divide_by > 1 else 1
 
-                                for param in model.parameters():
-                                    if args.std_theta != 0:
-                                        u_t = torch.normal(mean = 0, std = args.std_theta, size = param.shape).to(device)
-                                    else:
-                                        u_t = torch.zeros_like(param).to(device)
-                                    param.sub_(args.lr_theta * (param.grad/divide_by + u_t))
-                                
-                                if args.std_W != 0:
-                                    v_t = torch.normal(mean = 0, std = args.std_W, size = W.shape).to(device)
-                                else:
-                                    v_t = torch.zeros_like(W).to(device)
-                                W.add_(args.lr_W * (W.grad + v_t))
+                                #Updating per sample clipped gradient into the batch gradient of the model
+                                for name, param in model.named_parameters():
+                                    model_grad[name] += param.grad/divide_by
+                            
+                            ##### Per Sample Gradient Updates ######
 
-                                if not args.demographic_parity:
+                            counter += 1
+
+                            if counter == args.batch_size:
+                                with torch.no_grad():
+                                    for name, param in model.named_parameters():
+                                        if args.std_theta != 0:
+                                            u_t = torch.normal(mean = 0, std = args.std_theta, size = param.shape).to(device)
+                                        else:
+                                            u_t = torch.zeros_like(param).to(device)
+                                        param.sub_(args.lr_theta * (model_grad[name] + u_t))
+
                                     if args.std_W != 0:
-                                        v_t = torch.normal(mean = 0, std = args.std_W, size = W_.shape).to(device)
+                                        v_t = torch.normal(mean = 0, std = args.std_W, size = W.shape).to(device)
                                     else:
-                                        v_t = torch.zeros_like(W_).to(device)
-                                    W_.add_(args.lr_W * (W_.grad + v_t))
+                                        v_t = torch.zeros_like(W).to(device)
+                                    W.add_(args.lr_W * (W.grad + v_t))
 
-                                #Projecting W into a convex space
-                                norm_W = torch.norm(W.data)
-                                if norm_W > args.C:
-                                    W.copy_(args.C * W.data/norm_W)
-                                
-                                if not args.demographic_parity:
-                                    norm_W_ = torch.norm(W_.data)
-                                    if norm_W_ > args.C:
-                                        W_.copy_(args.C * W_.data/norm_W_)
+                                    if not args.demographic_parity:
+                                        if args.std_W != 0:
+                                            v_t = torch.normal(mean = 0, std = args.std_W, size = W_.shape).to(device)
+                                        else:
+                                            v_t = torch.zeros_like(W_).to(device)
+                                        W_.add_(args.lr_W * (W_.grad + v_t))
+
+                                    #Projecting W into a convex space
+                                    norm_W = torch.norm(W.data)
+                                    if norm_W > args.C:
+                                        W.copy_(args.C * W.data/norm_W)
+                                    
+                                    if not args.demographic_parity:
+                                        norm_W_ = torch.norm(W_.data)
+                                        if norm_W_ > args.C:
+                                            W_.copy_(args.C * W_.data/norm_W_)
+                                    
+                                    if W.grad is not None:
+                                        W.grad.copy_(torch.zeros(W.shape))
+                                    if not args.demographic_parity:
+                                        if W_.grad is not None:
+                                            W_.grad.copy_(torch.zeros(W_.shape))
+
+                                    #Resetting the model gradients accumulated across the batch
+                                    for name, param in model.named_parameters():
+                                        model_grad[name] = torch.zeros(param.shape).to(device)
+                                    
+                                counter = 0         #Resetting the sample counter for next batch update
                     
                     model.eval()
 
